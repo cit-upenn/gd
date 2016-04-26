73e1f5e588e07d1d58ae97b68f74df7a2e319f8c
Qingxiao Dong <qingxiao.dong@gmail.com>
2016-04-25 21:38:53 -0400

    words import to database

--- sqlite/DefinitionParser.java
+++ sqlite/DefinitionParser.java
@@ -10,12 +10,12 @@ public class DefinitionParser {
 		
 	public static void main (String[] args) {
 		
-		// error handling
-		if (args.length != 1) {
-			System.out.println("Please input one directory: dir of the dictionary file.");
-			return;
-		}
-		
+	    // error handling
+        if (args.length != 1) {
+            System.out.println("Please input one directory: dir of the dictionary file.");
+            return;
+        }
+        
         String fileName = args[0];                               // directory where the dictionary is stored
         
         Map<String, Word> words = new HashMap<String, Word>();   // hashmap to store the string-word pair
@@ -25,7 +25,7 @@ public class DefinitionParser {
         String currentWord = null;                               // the current vocab entry we are looking at (work as a pointer)
 
         try {
-        	
+            
             FileReader fileReader = new FileReader(fileName);
 
             BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -35,62 +35,64 @@ public class DefinitionParser {
             Word current = null;                                 // the word obj of the current vocab (work as a pointer)
             
             while(line != null) {
-            	if (line.matches("^[A-Z-]+")) {                  // first step: match the current vocab
-            		
-            		currentWord = line;
-            		current = words.get(currentWord);            // try to get the corresponding word obj of the current entry
-            		if (current == null) {                       // if it dne in our hashmap, 
-            			Word newWord = new Word(currentWord);    // generate a word obj for it,
-            			words.put(currentWord, newWord);         // and update our hashmap 
-            			current = newWord;                       // update the current word obj pointer
-            		}
-            		
+                if (line.matches("^[A-Z-]+")) {                  // first step: match the current vocab
+                    
+                    currentWord = line;
+                    current = words.get(currentWord);            // try to get the corresponding word obj of the current entry
+                    if (current == null) {                       // if it dne in our hashmap, 
+                        Word newWord = new Word(currentWord);    // generate a word obj for it,
+                        words.put(currentWord, newWord);         // and update our hashmap 
+                        current = newWord;                       // update the current word obj pointer
+                    }
+                    
                 } else if (line.matches("^[0-9]{1,2}[. (]{3}[^\n]+")) {
-                	                                             // if matches pattern such as: 1. (law), skip it.  
+                                                                 // if matches pattern such as: 1. (law), skip it.  
                 } else if (line.matches("^[0-9]{1,2}[.][^\n]+")) { 
-                												 // second step: match the defn that starts with number.
-                	String definition = "";
-                	while (line.matches("[^\n]+")) {             // concatenate the sentences in that defn together,
-                		definition += line + " ";                // since even inside one defn, parts are separated by \n.
-                		line = bufferedReader.readLine();        // keep concatenating until we see a blank line.
-                	}
-                	
-                	// System.out.println("[Number Def] " + definition); // for debugging
-                	
-                	int indexOfSpace = definition.indexOf(" ");  // find the first occurrence of " "
-                	String trimmedDefn = definition.substring(indexOfSpace+1);
-                	current.addDefinition(trimmedDefn);          // add this defn to the current word's defn list
-                	
+                                                                 // second step: match the defn that starts with number.
+                    String definition = "";
+                    while (line.matches("[^\n]+")) {             // concatenate the sentences in that defn together,
+                        definition += line + " ";                // since even inside one defn, parts are separated by \n.
+                        line = bufferedReader.readLine();        // keep concatenating until we see a blank line.
+                    }
+                    
+                    // System.out.println("[Number Def] " + definition); // for debugging
+                    
+                    int indexOfSpace = definition.indexOf(" ");  // find the first occurrence of " "
+                    String trimDef = definition.substring(indexOfSpace+1);
+                    String finalDef = trimDef.replace("'", "''");       // replace all single quotes by escape char
+                    current.addDefinition(finalDef);             // add this defn to the current word's defn list
+                    
                 }  else if (line.matches("^Defn:[^\n]+")) {      // third step: match the defn that starts with "Defn".
-                	
-                	String definition = "";                   
-                	while (line.matches("[^\n]+")) {             // same as above
-                		definition += line;
-                		line = bufferedReader.readLine();
-                	}
-                	
-                	// System.out.println("[Number Def] " + definition);
-                	current.addDefinition(definition);
-                	
+                    
+                    String definition = "";                   
+                    while (line.matches("[^\n]+")) {             // same as above
+                        definition += line;
+                        line = bufferedReader.readLine();
+                    }
+                    
+                    // System.out.println("[Number Def] " + definition);
+                    String finalDef = definition.replace("'", "''");    // replace all single quotes by escape char
+                    current.addDefinition(finalDef);
+                    
                 }
-            	
+                
                 line = bufferedReader.readLine();    
             }   
             
             bufferedReader.close();
             
         } catch(FileNotFoundException ex) {
-        	
+            
             System.out.println( "Unable to open file '" + fileName + "'");
             
         } catch(IOException ex) {
-        	
+            
             System.out.println("Error reading file '" + fileName + "'");   
             
         }
         
         for (String word : words.keySet()) {
-        	SQLiteJDBC.insertIntoDictionary(words.get(word));
+    	   SQLiteJDBC.insertIntoDictionary(words.get(word));
         }
 
 	}
--- sqlite/SQLiteTester.java
+++ sqlite/SQLiteTester.java
@@ -11,35 +11,38 @@ public class SQLiteTester {
      */
     public static void main(String[] args) {
 
-        /** test insert into dictionary  */
-        Word word = new Word("connection");
-        word.addDefinition("a relationship in which a person, thing, or idea is linked or associated with something else.");
-        word.addDefinition("a supplier of narcotics.");
-        SQLiteJDBC.insertIntoDictionary(word);
+        // /** test insert into dictionary  */
+        // Word word = new Word("connection");
+        // word.addDefinition("a relationship in which a person, thing, or idea is linked or associated with something else.");
+        // word.addDefinition("a supplier of narcotics.");
+        // SQLiteJDBC.insertIntoDictionary(word);
 
-        /** test select from dictionary */
-        Word w = SQLiteJDBC.selectFromDictionary("main");
-        w.print();
-        SQLiteJDBC.selectFromDictionary("application").print();
+        // /** test select from dictionary */
+        // Word w = SQLiteJDBC.selectFromDictionary("main");
+        // w.print();
+        // SQLiteJDBC.selectFromDictionary("application").print();
 
-        /** test add word to learn and get words to learn */
-        SQLiteJDBC.addWordToLearn("application");
-        SQLiteJDBC.addWordToLearn(word);
-        ArrayList<Word> words = SQLiteJDBC.getWordsToLearn();
-        for (Word wd : words) {
-            wd.print();
-        }
+        // /** test add word to learn and get words to learn */
+        // SQLiteJDBC.addWordToLearn("application");
+        // SQLiteJDBC.addWordToLearn(word);
+        // ArrayList<Word> words = SQLiteJDBC.getWordsToLearn();
+        // for (Word wd : words) {
+        //     wd.print();
+        // }
 
-        /** test remove word to learn and get words to learn */
-        SQLiteJDBC.removeWordToLearn(word);
-        words = SQLiteJDBC.getWordsToLearn();
-        for (Word wd : words) {
-            wd.print();
-        }
-        SQLiteJDBC.removeWordToLearn("application");
-        words = SQLiteJDBC.getWordsToLearn();
-        for (Word wd : words) {
-            wd.print();
-        }
+        // /** test remove word to learn and get words to learn */
+        // SQLiteJDBC.removeWordToLearn(word);
+        // words = SQLiteJDBC.getWordsToLearn();
+        // for (Word wd : words) {
+        //     wd.print();
+        // }
+        // SQLiteJDBC.removeWordToLearn("application");
+        // words = SQLiteJDBC.getWordsToLearn();
+        // for (Word wd : words) {
+        //     wd.print();
+        // }
+
+        SQLiteJDBC.selectFromDictionary("A").print();
+
     }
 }
