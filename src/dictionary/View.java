d45f3ca33f83c75ed0f82957c51783ef837f5d07
lj1010 <ljue1010@gmail.com>
2016-05-02 20:39:22 -0400

    use JTextPane instead of JTextArea in View, modified html format display

--- src/dictionary/View.java
+++ src/dictionary/View.java
@@ -1,23 +1,14 @@
 package dictionary;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.Panel;
-import java.awt.TextArea;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
-import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
-import javax.swing.SwingConstants;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -30,10 +21,12 @@ public class View extends Panel implements ListSelectionListener {
 	private Model model;
 	private DefaultListModel<String> listModel;
 	private JList<String> wordsList;
-	private JTextArea defArea;
+	// private JTextArea defArea;
+	private JTextPane textPane;
 	private String selectedWord;
 	private ArrayList<String> candidateWords;
 	private JScrollPane textScroller;
+	private String initialText;
 
 	public View(Model model) {
 		this.model = model;
@@ -53,14 +46,21 @@ public class View extends Panel implements ListSelectionListener {
 		// listScroller = new JScrollPane(wordsList);
 		// listScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		// listScroller.setPreferredSize(new Dimension(500, 500));
-		defArea = new JTextArea();
-		defArea.setEditable(false);
-		defArea.setLineWrap(true);
-		defArea.setWrapStyleWord(true);
-		defArea.setBorder(
-				BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLACK), BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-		defArea.setFont(new Font("Helvetica", Font.PLAIN, 14));
 
+		// defArea = new JTextArea();
+		// defArea.setEditable(false);
+		// defArea.setLineWrap(true);
+		// defArea.setWrapStyleWord(true);
+		// defArea.setBorder(
+		// BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLACK),
+		// BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+		// defArea.setFont(new Font("Helvetica", Font.PLAIN, 14));
+		textPane = new JTextPane();
+		initialText = "<html><body> <p> Welcome to our dictionary! </p></body></html>";
+		textPane.setContentType("text/html");
+		textPane.setText(initialText);
+		textScroller = new JScrollPane(textPane);
+		
 		candidateWords = new ArrayList<String>();
 
 		layOutComponents();
@@ -70,23 +70,7 @@ public class View extends Panel implements ListSelectionListener {
 	private void layOutComponents() {
 		this.setLayout(new BorderLayout());
 		this.add(BorderLayout.WEST, wordsList);
-		this.add(BorderLayout.CENTER, defArea);
-		
-//		this.setLayout(new BorderLayout());
-//		this.add(BorderLayout.WEST, wordsList);
-//		
-//		String initialText = MerriamWebsterAPI.getThesaurusHtml("happy");
-//		JTextArea htmlTextArea = new JTextArea(10, 20);
-//		htmlTextArea.setText(initialText);
-//		JLabel theLabel = new JLabel(initialText);
-//		theLabel.setVerticalAlignment(SwingConstants.CENTER);
-//		theLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//		JPanel rightPanel = new JPanel();
-//		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
-//		rightPanel.add(theLabel);
-//		rightPanel.setBackground(Color.WHITE);
-//		JScrollPane textScroller = new JScrollPane(rightPanel);
-//		this.add(BorderLayout.CENTER, textScroller);
+		this.add(BorderLayout.CENTER, textScroller);
 	}
 
 	public void updateWords(String starter) {
@@ -118,21 +102,23 @@ public class View extends Panel implements ListSelectionListener {
 		if (wordStr.trim().length() == 0) {
 			return;
 		}
-		defArea.setText("");
+		String text = "<html><body>";
 		Word word = SQLiteJDBC.selectFromDictionary(wordStr.toUpperCase());
-		if (word.getDefinitions().size()==0) {
-			defArea.append("No such word");
-			return;
+		if (word.getDefinitions().size() == 0) {
+			text += "<p>No such word!</p></body></html>";
+		} else {
+			selectedWord = wordStr.toLowerCase();
+			text += selectedWord;
+			text += "<br><br>Definitions:<br><br>";
+			for (int i = 0; i < word.getDefinitions().size(); i++) {
+				text += (i + 1) + ". ";
+				text += word.getDefinitions().get(i);
+				text += "<br><br>";
+			}
 		}
-		selectedWord = wordStr.toLowerCase();
-		defArea.append(selectedWord);
-		defArea.append("\n\nDefinitions:\n\n");
-		
-		for (int i = 0; i < word.getDefinitions().size(); i++) {
-			defArea.append("" + (i + 1) + ". ");
-			defArea.append(word.getDefinitions().get(i));
-			defArea.append("\n\n");
-		}
+		textPane.setText(text);
+		this.validate();
+
 	}
 
 	public void backToDefinitions() {
@@ -140,29 +126,20 @@ public class View extends Panel implements ListSelectionListener {
 	}
 
 	private void cleanView() {
-		defArea.setText("");
+		textPane.setText(initialText);
+		this.validate();
 	}
 
 	public void appendTranslation() {
 		String chinese = model.getChinese(selectedWord);
-		defArea.setText("");
-		defArea.append(chinese);
+		textPane.setText(chinese);
+		this.validate();
 	}
 
 	public void getThesaurus() {
-		String initialText = MerriamWebsterAPI.getThesaurusHtml("happy");
-		JTextArea htmlTextArea = new JTextArea(10, 20);
-		htmlTextArea.setText(initialText);
-		JLabel theLabel = new JLabel(initialText);
-		theLabel.setVerticalAlignment(SwingConstants.CENTER);
-		theLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		JPanel rightPanel = new JPanel();
-		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
-		rightPanel.add(theLabel);
-		rightPanel.setBackground(Color.GRAY);
-		textScroller = new JScrollPane(rightPanel);
-		this.remove(defArea);
-		this.add(BorderLayout.CENTER, textScroller);
+		String initialText = MerriamWebsterAPI.getThesaurusHtml(selectedWord);
+		System.out.println(initialText);
+		textPane.setText(initialText);
 		this.validate();
 	}
 
